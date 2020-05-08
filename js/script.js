426b812f1c646af5ccc6b624d2fe89a755232036
andrewchenz <hello@an.drew.sg>
2020-05-08 17:11:01 +0800

    Vanilla smooth scroll works

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-console.log("hello world!");
+// console.log("hello world!");
 
-let finalContent;
-let reviewContent;
+// let finalContent = "";
+// let reviewContent = "";
 
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+function scrollToSection(evt) {
+    evt.preventDefault();
+    console.log(`You've just clicked ${evt.target.textContent}`);
+    console.log(evt.target.getAttribute('href'));
+    document.querySelector(evt.target.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+document.querySelector("nav").addEventListener('click', scrollToSection);
